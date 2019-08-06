1fce15f87e44b5ac993549f21509bc271bdd06dd
Calvin Beck <hobbes@ualberta.ca>
2019-08-06 10:08:49 -0400

    Add org-babel LLVM file.

---
+++ ob-llvm.el
@@ -0,0 +1,86 @@
+;;; ob-llvm.el --- org-babel functions for llvm evaluation
+
+;; Copyright (C) Calvin Beck
+
+;; Author: Calvin Beck
+;; Keywords: literate programming, reproducible research, llvm
+;; Homepage: https://orgmode.org
+;; Version: 0.01
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Requirements:
+;;; Code:
+(require 'ob)
+(require 'ob-ref)
+(require 'ob-comint)
+(require 'ob-eval)
+;; possibly require modes required for your language
+
+;; optionally define a file extension for this language
+(add-to-list 'org-babel-tangle-lang-exts '("llvm" . "ll"))
+
+;; optionally declare default header arguments for this language
+(defvar org-babel-default-header-args:llvm '())
+
+;; This is the main function which is called to evaluate a code
+;; block.
+;;
+;; This function will evaluate the body of the source code and
+;; return the results as emacs-lisp depending on the value of the
+;; :results header argument
+;; - output means that the output to STDOUT will be captured and
+;;   returned
+;; - value means that the value of the last statement in the
+;;   source code block will be returned
+;;
+;; The most common first step in this function is the expansion of the
+;; PARAMS argument using `org-babel-process-params'.
+;;
+;; Please feel free to not implement options which aren't appropriate
+;; for your language (e.g. not all languages support interactive
+;; "session" evaluation).  Also you are free to define any new header
+;; arguments which you feel may be useful -- all header arguments
+;; specified by the user will be available in the PARAMS variable.
+(defun org-babel-execute:llvm (body params)
+  "Execute a block of Llvm code with org-babel.
+This function is called by `org-babel-execute-src-block'"
+  (message "executing Llvm source code block")
+  (let* ((processed-params (org-babel-process-params params))
+         (pass-arg (cdr (assq :pass processed-params)))
+         (pass (if pass-arg pass-arg "instsimplify"))
+         (full-body (org-babel-expand-body:generic
+                     body params)))
+    ;; actually execute the source-code block either in a session or
+    ;; possibly by dropping it to a temporary file and evaluating the
+    ;; file.
+    ;; 
+    ;; for session based evaluation the functions defined in
+    ;; `org-babel-comint' will probably be helpful.
+    ;;
+    ;; for external evaluation the functions defined in
+    ;; `org-babel-eval' will probably be helpful.
+    ;;
+    ;; when forming a shell command, or a fragment of code in some
+    ;; other language, please preprocess any file names involved with
+    ;; the function `org-babel-process-file-name'. (See the way that
+    ;; function is used in the language files)
+    (org-babel-eval (format "opt -%s -S" pass) body)))
+
+(provide 'ob-llvm)
+;;; ob-llvm.el ends here
